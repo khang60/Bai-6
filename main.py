295df8451c950c1cf657556b40ddb114088cb093
Lương  Khang <26802941-luongkhang17102@users.noreply.replit.com>
2024-11-17 02:37:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import time
+st.title('my progress')
+my_bar = st.progress(0)
+for percent_complete in range(100):
+  time.sleep(0.05)
+  my_bar.progress(percent_complete + 1)
+
+if st.button("Click me"):
+  st.balloons()
